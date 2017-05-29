a7e013642049202dcf98a365c504b6ec159fafad
chornge <christianmbaba@live.com>
2017-05-29 01:33:07 -0400

    Add UnitTests. Add Mockito dependency.

---
+++ app/src/test/java/com/coaster/android/coaster/UnitTests.java
@@ -0,0 +1,28 @@
+package com.coaster.android.coaster;
+
+import android.content.Context;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UnitTests {
+
+    @Mock
+    Context mockContext;
+    MainActivity mainActivity;
+    private DrinksFragment drinksFragment = new DrinksFragment();
+    private CategoryFragment categoryFragment = new CategoryFragment();
+
+    @Test
+    public void vodkaStringKeyTest() throws Exception {
+
+    }
+
+    @Test
+    public void whiskeyStringKeyTest() throws Exception {
+
+    }
+}
