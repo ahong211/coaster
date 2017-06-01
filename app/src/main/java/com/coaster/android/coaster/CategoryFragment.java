0506b6220dc56b21493ac7abf1561c91fe93b0b8
chornge <christianmbaba@live.com>
2017-05-31 22:13:45 -0400

    Remove addition of storage images. Tidyup.

--- app/src/main/java/com/coaster/android/coaster/CategoryFragment.java
+++ app/src/main/java/com/coaster/android/coaster/CategoryFragment.java
@@ -1,9 +1,6 @@
 package com.coaster.android.coaster;
 
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
@@ -12,13 +9,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
-
-import java.io.ByteArrayOutputStream;
 
 public class CategoryFragment extends Fragment implements View.OnClickListener {
 
@@ -45,9 +36,6 @@ public class CategoryFragment extends Fragment implements View.OnClickListener {
 
         View view = inflater.inflate(R.layout.fragment_category, container, false);
 
-        storageReference = FirebaseStorage.getInstance().getReference();
-        StorageReference imageReference = storageReference.child("Images");
-
         ginButton = (ImageView) view.findViewById(R.id.gin_button);
         rumButton = (ImageView) view.findViewById(R.id.rum_button);
         tequilaButton = (ImageView) view.findViewById(R.id.tequila_button);
@@ -55,13 +43,12 @@ public class CategoryFragment extends Fragment implements View.OnClickListener {
         whiskeyButton = (ImageView) view.findViewById(R.id.whiskey_button);
         customDrinksButton = (Button) view.findViewById(R.id.custom_drinks_button);
 
-        saveGinImageToStorage(imageReference, ginButton);
-        saveRumImageToStorage(imageReference, rumButton);
-        saveTequilaImageToStorage(imageReference, tequilaButton);
-        saveVodkaImageToStorage(imageReference, vodkaButton);
-        saveWhiskeyImageToStorage(imageReference, whiskeyButton);
+        //storageReference = FirebaseStorage.getInstance().getReference();
+        //StorageReference imageReference = storageReference.child("Images");
 
-        // TODO: Add mixed drink image to Firebase Storage.
+        /* This is for saving images to firebase storage.
+            saveImageToStorage(imageReference, image_button);
+        */
 
         customDrinksButton.setOnClickListener(this);
         ginButton.setOnClickListener(this);
@@ -73,64 +60,40 @@ public class CategoryFragment extends Fragment implements View.OnClickListener {
         return view;
     }
 
-    private void saveGinImageToStorage(StorageReference imageReference, ImageView gin) {
-        StorageReference ginReference = imageReference.child("gin.png");
-        byte[] data = convertImageToBytes(gin);
-        uploadImageToStorage(ginReference, data);
-    }
-
-    private void saveRumImageToStorage(StorageReference imageReference, ImageView rum) {
-        StorageReference rumReference = imageReference.child("rum.png");
-        byte[] data = convertImageToBytes(rum);
-        uploadImageToStorage(rumReference, data);
-    }
-
-    private void saveTequilaImageToStorage(StorageReference imageReference, ImageView tequila) {
-        StorageReference tequilaReference = imageReference.child("tequila.png");
-        byte[] data = convertImageToBytes(tequila);
-        uploadImageToStorage(tequilaReference, data);
-    }
-
-    private void saveVodkaImageToStorage(StorageReference imageReference, ImageView vodka) {
-        StorageReference vodkaReference = imageReference.child("vodkatransparent.png");
-        byte[] data = convertImageToBytes(vodka);
-        uploadImageToStorage(vodkaReference, data);
-    }
-
-    private void saveWhiskeyImageToStorage(StorageReference imageReference, ImageView whiskey) {
-        StorageReference whiskeyReference = imageReference.child("whiskey.png");
-        byte[] data = convertImageToBytes(whiskey);
-        uploadImageToStorage(whiskeyReference, data);
-    }
-
-    private byte[] convertImageToBytes(ImageView img) {
-        int qualityOfImage = 100;
-        img.setDrawingCacheEnabled(true);
-        img.buildDrawingCache();
-
-        Bitmap bitmap = ((BitmapDrawable) img.getDrawable()).getBitmap();
-        ByteArrayOutputStream arrayOutputStream = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.PNG, qualityOfImage, arrayOutputStream);
-
-        return arrayOutputStream.toByteArray();
-    }
-
-    private void uploadImageToStorage(StorageReference reference, byte[] data) {
-        UploadTask task = reference.putBytes(data);
-        task.addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                // Handle unsuccessful uploads.
-            }
-
-        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-            @Override
-            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                // Handle successful uploads.
-            }
-
-        });
-    }
+//    private void saveImageToStorage(StorageReference imageReference, ImageView img) {
+//        StorageReference reference = imageReference.child("gin.png");
+//        byte[] data = convertImageToBytes(img);
+//        uploadImageToStorage(reference, data);
+//    }
+//
+//    private byte[] convertImageToBytes(ImageView img) {
+//        int qualityOfImage = 100;
+//        img.setDrawingCacheEnabled(true);
+//        img.buildDrawingCache();
+//
+//        Bitmap bitmap = ((BitmapDrawable) img.getDrawable()).getBitmap();
+//        ByteArrayOutputStream arrayOutputStream = new ByteArrayOutputStream();
+//        bitmap.compress(Bitmap.CompressFormat.PNG, qualityOfImage, arrayOutputStream);
+//
+//        return arrayOutputStream.toByteArray();
+//    }
+//
+//    private void uploadImageToStorage(StorageReference reference, byte[] data) {
+//        UploadTask task = reference.putBytes(data);
+//        task.addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull Exception e) {
+//                // Handle unsuccessful uploads.
+//            }
+//
+//        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+//            @Override
+//            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//                // Handle successful uploads.
+//            }
+//
+//        });
+//    }
 
     @Override
     public void onClick(View v) {
