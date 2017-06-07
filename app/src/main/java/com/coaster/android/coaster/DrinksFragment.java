d505baea3105cc3a1fa65f21fc014c04927dcfbd
chornge <christianmbaba@live.com>
2017-06-07 01:43:59 -0400

    Add drink images dynamically from database.

--- app/src/main/java/com/coaster/android/coaster/DrinksFragment.java
+++ app/src/main/java/com/coaster/android/coaster/DrinksFragment.java
@@ -86,6 +86,7 @@ public class DrinksFragment extends Fragment {
 
                     mCocktailList.add(value);
                 }
+                
                 mLayoutManager = new LinearLayoutManager(getContext());
 
                 mRecyclerView.setLayoutManager(mLayoutManager);
