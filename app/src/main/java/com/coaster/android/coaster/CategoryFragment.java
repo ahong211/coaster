db7a1a4a3524e07bd0d5a2a4b13728614b344bd3
Kevin <kburo21@gmail.com>
2017-06-07 03:26:46 -0400

    Add sidebar nav drawer. Add options menu.

--- app/src/main/java/com/coaster/android/coaster/CategoryFragment.java
+++ app/src/main/java/com/coaster/android/coaster/CategoryFragment.java
@@ -30,6 +30,7 @@ public class CategoryFragment extends Fragment implements View.OnClickListener {
         // Required empty public constructor
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
